96b5a7a75cd508eb5aa459618b380c35240a720d
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-10-23 14:15:02 -0600

    Create Customer class

---
+++ CustomerConsoleDB/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerConsoleDB
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        [MaxLength(50)]
+        public string Email { get; set; }
+    }
+}
