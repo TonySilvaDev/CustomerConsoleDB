8c2c85fc2919adcb7f2dd4894a83b009f1bf3d61
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-10-23 14:40:23 -0600

    Create data base with code first

---
+++ CustomerConsoleDB/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerConsoleDB.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Customer> Customer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfigurationBuilder configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            IConfiguration configuration = configBuilder.Build();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
