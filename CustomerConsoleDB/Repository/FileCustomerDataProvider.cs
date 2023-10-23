4873afef72bd1174aec736a2e54ea996c3981287
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-10-23 14:22:05 -0600

    Imlement interface in FileCustomerDataProvider

---
+++ CustomerConsoleDB/Repository/FileCustomerDataProvider.cs
@@ -0,0 +1,32 @@
+﻿using CustomerConsoleDB.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerConsoleDB.Repository
+{
+    public class FileCustomerDataProvider : ICustomerDataProvider
+    {
+        public List<Customer> GetCustomerList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomersListByAgeRange(int startAge, int endAge)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveCustomerList(List<Customer> customerList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
