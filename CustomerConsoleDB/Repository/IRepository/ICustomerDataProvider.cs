2acd4313b04c1266c4aa1f54cfc454a2697f22e8
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-10-23 14:20:19 -0600

    Create Interface ICustomerDataProvider

---
+++ CustomerConsoleDB/Repository/IRepository/ICustomerDataProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerConsoleDB.Repository.IRepository
+{
+    public interface ICustomerDataProvider
+    {
+        List<Customer> GetCustomerList();
+        List<Customer> GetCustomersListByAgeRange(int startAge, int endAge);
+        void SaveCustomer(Customer customer);
+        void SaveCustomerList(List<Customer> customerList);
+    }
+}
